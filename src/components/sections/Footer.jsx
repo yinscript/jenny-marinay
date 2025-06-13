d2f61ddd4696fe9afd3c9184686ba480df279712
Jeni <92709282+jennymarinay@users.noreply.github.com>
2025-06-13 14:18:51 +0800

    fixed footer responsiveness

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -3,8 +3,11 @@ import { socialLinks } from '/src/data/socialLinks';
 const Footer = () => {
   return (
     <footer className="max-w-4xl px-4 pt-15 pb-8 mx-auto flex items-center justify-between">
-      <p className="text-md text-zinc-400">&#169; 2025 Jenny Marinay</p>
-      <div className="flex items-center gap-2 pt-5">
+      <p className="text-md text-zinc-400">
+        &#169; 2025 <span className="inline sm:hidden">JM</span>
+        <span className="hidden sm:inline">Jenny Marinay</span>
+      </p>
+      <div className="flex items-center gap-2">
         {socialLinks.map(({ url, icon: Icon, name }) => (
           <a
             key={name}
@@ -13,7 +16,7 @@ const Footer = () => {
             rel="noopener noreferrer"
             aria-label={name}
           >
-            <Icon className="text-2xl text-zinc-500 hover:text-zinc-300" />
+            <Icon className="text-2xl text-zinc-500 hover:text-zinc-300 transition-colors duration-300" />
           </a>
         ))}
       </div>
